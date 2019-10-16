005eaeff15711e5e5ccbc7d7b201db49e0dd148f
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-10-16 16:07:17 +0530

    Update Assignment_6.cpp

--- Assignment_6.cpp
+++ Assignment_6.cpp
@@ -2,152 +2,165 @@
 ==================================
  ASSIGNMENT 6 : Student Database
 ==================================
-
 Name : Sahil Hadke
 Roll No. : 17
 Batch : S2
-
 Develop an object oriented program in C++ to create a database of student information
 system containing the following information: Name, Roll number, Class, division, Date of
 Birth, Blood group, Contact address, telephone number, driving license no. etc Construct the
 database with suitable member functions for initializing and destroying the data viz
-constructor, default constructor, Copy constructor, destructor, static member functions, friend
-class, this pointer, in-line code and dynamic memory allocation operators-new and delete.
+
+constructor,
+default constructor,
+Copy constructor,
+destructor,
+static member functions,
+friend class,
+this pointer,
+in-line code,
+dynamic memory allocation operators-new and delete.
+
 */
 
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<cstring>
 using namespace std;
 
-class DateOfBirth
+class DOB
 {
 public:
-    int Day, Month, Year;
-
-    DateOfBirth()
-    {
-        //default constructor
-    }
-
-    DateOfBirth(DateOfBirth &obj) //Copy Constructor
-    {
-        Day = obj.Day;
-        Month = obj.Month;
-        Year = obj.Year;
-    }
+    int d, m, y;
 
     void getData()
     {
-        cout<<"Date of Birth"<<endl;
-        cout<<"Enter Day(int) :";
-        cin>>Day;
-        cout<<"Enter Month(int) :";
-        cin>>Month;
-        cout<<"Enter Year(int) :";
-        cin>>Year;
+        cout<<"Enter date of birth :";
+        cin>>d;
+        cout<<"Enter month of birth :";
+        cin>>m;
+        cout<<"Enter year of birth :";
+        cin>>y;
+    }
+    DOB()
+    {
+        d = 0;
+        m = 0;
+        y = 0;
     }
 
-    void PrintData()
+    DOB(DOB &obj)
     {
-        cout<<"DOB              : "<<Day<<"/"<<Month<<"/"<<Year<<endl;
+        d = obj.d;
+        m = obj.m;
+        y = obj.y;
+    }
+
+    void printData()
+    {
+        cout<<"Date of birth :"<<d<<"/"<<m<<"/"<<y<<endl;
     }
 };
 
 class Student
 {
 public:
+    static int sCount;
+    string name, div, bg, ph, add, lic, _class;
+    int rn;
+    DOB *dob;
 
-    static int studentCount;
-    string name, _class, blood, address, tele_num, lic_num;
-    char division;
-    int roll;
-    DateOfBirth DOB;
-
-    Student() //Default Constructor
+    Student()
     {
-        studentCount++;
+        Student::sCount++;
     }
 
-    Student(Student &obj) //Copy Constructor
+    Student(Student &obj)
     {
+        sCount++;
         name = obj.name;
-        _class = obj._class;
-        blood = obj.blood;
-        division = obj.division;
-        roll = obj.roll;
-        address = obj.address;
-        tele_num = obj.tele_num;
-        lic_num = obj.lic_num;
-        DOB = obj.DOB;
-        studentCount++;
+        bg=obj.bg;
+        ph=obj.ph;
+        add=obj.add;
+        lic=obj.lic;
+        rn = obj.rn;
+        _class=obj._class;
+        dob = new DOB(*obj.dob);
     }
 
-    ~Student() //Destructor
-    {
-        studentCount--;
+    ~Student(){
+         Student::sCount--;
     }
 
     void getData();
     void printData();
 };
 
-int Student::studentCount = 0;
-
 void Student::getData()
 {
-    cout<<"Enter student name(string) :";
+    cout<<"\nName :";
     getline(cin, name);
-    cout<<"Enter student address(string) :";
-    getline(cin, address);
-    cout<<"Enter student class(string) :";
-    getline(cin, _class);
-    cout<<"Enter blood group(string) :";
-    getline(cin, blood);
-    cout<<"Enter student division(char) :";
-    cin>>division;
-    cout<<"Enter student roll number(int) :";
-    cin>>roll;
+
+    cout<<"\nRoll No :";
+    cin>>rn;
+
+    cout<<"\nClass :";
     cin.ignore();
-    cout<<"Enter student telephone number(string) :";
-    getline(cin, tele_num);
-    cout<<"Enter student license number(string) :";
-    getline(cin, lic_num);
-    DOB.getData();
+    getline(cin ,_class);
+
+    cout<<"\nDiv :";
+    getline(cin ,div);
+
+    cout<<"\nBlood Group :";
+    getline(cin , bg);
+
+    cout<<"\nPhone No :";
+    getline(cin ,ph);
+
+    cout<<"\nLicense Number :";
+    getline(cin ,lic);
+
+    cout<<"\nAddress :";
+    getline(cin ,add);
+
+    dob = new DOB;
+    dob->getData();
 }
 
 void Student::printData()
 {
-    cout<<endl;
-    cout<<"STUDENT INFORMATION"<<endl<<endl;
-    cout<<"Name             : "<<name<<endl;
-    cout<<"Roll No          : "<<roll<<endl;
-    cout<<"Class            : "<<_class<<endl;
-    cout<<"Division         : "<<division<<endl;
-    cout<<"Address          : "<<address<<endl;
-    cout<<"Telephone Number : "<<tele_num<<endl;
-    cout<<"License Number   : "<<lic_num<<endl;
-    cout<<"Blood Group      : "<<blood<<endl;
-    DOB.PrintData();
-    cout<<"Total No.        : "<<studentCount<<endl;
-    cout<<endl;
+    cout<<"Name :"<<name<<endl;
+
+    cout<<"Roll No :"<<rn<<endl;
+
+    cout<<"Class :"<<_class<<endl;
+
+    cout<<"Div :"<<div<<endl;
+
+    cout<<"Blood Group :"<<bg<<endl;
+
+    cout<<"Phone No :"<<ph<<endl;
+
+    cout<<"License Number :"<<lic<<endl;
+
+    cout<<"Address :"<<add<<endl;
+
+    dob->printData();
 }
+
+int Student::sCount = 0;
 
 int main()
 {
-    Student *s1 = new Student; //using new operator to instantiate.
+
+
+    Student *s1 = new Student;
+
+    cout<<"S ="<<Student::sCount;
+
     s1->getData();
-    cout<<"Printing data of s1"<<endl;
     s1->printData();
 
-    Student *s2 = new Student(*s1); //copy constructor
-    cout<<"Printing data of s2"<<endl;
+    Student *s2 = new Student(*s1);
     s2->printData();
-
-    delete s2;
-    delete s1;
-
-    cout<<"Both students deleted"<<endl;
-    cout<<"students count : "<<Student::studentCount<<endl;
 
     return 0;
 }
