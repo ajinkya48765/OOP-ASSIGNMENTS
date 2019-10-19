750343c23f56141d73e7b5a77d8ff31ff946001b
Ajinkya S Khandave <52904749+ajinkya48765@users.noreply.github.com>
2019-10-19 14:16:16 +0530

    Update Assignment_14.cpp
    Implementation of stack and queue using STL, means that we have to use standard template  libraries stack and queue to do various operations on stack and queue in a single program.
    I am suggesting you more optimized and specific code, if you agree do approve it.

--- Assignment_14.cpp
+++ Assignment_14.cpp
@@ -1,110 +1,159 @@
-/*
-    STACK AND QUEUE USING STL
-*/
 
-#include<iostream>
-#include<cstdlib>
-#include<list>
-#include<iterator>
+//Program to implement stack and queue using SSL
+
+
+
+
+
+
+#include <algorithm> 
+#include <stack> 
+#include <queue>
+#include <iostream> 
+#include <iterator> 
+#include <string>
 using namespace std;
 
-class QUEUE
-{
-public:
-    list<int> lst;
+int showstack(stack <int>);
 
-    void enqueue(int a);
-    void dequeue();
-    void display();
-};
-
-void QUEUE::display()
-{
-    list<int>::iterator st, en;
-    st = lst.begin();
-    en = lst.end();
-
-    if(st == en)
-    {
-        cout<<"EMPTY"<<endl;
-    }
-    else
-    {
-        cout<<"DISPLAYING QUEUE"<<endl;
-        while(st != en)
-        {
-            cout<<*st<<" ";
-            st++;
-        }
-        cout<<endl;
-    }
-}
-
-void QUEUE::dequeue()
-{
-    lst.pop_front();
-}
-
-void QUEUE::enqueue(int a)
-{
-    lst.push_back(a);
-}
-
-class STACK
-{
-public:
-    list<int>lst;
-
-    void display();
-    void push(int a);
-    void pop();
-};
-
-void STACK::display()
-{
-    list<int>::iterator st, en;
-    st = lst.begin();
-    en = lst.end();
-
-    if(st == en)
-    {
-        cout<<"EMPTY"<<endl;
-    }
-    else
-    {
-        cout<<"DISPLAYING STACK"<<endl;
-        while(st != en)
-        {
-            cout<<*st<<" ";
-            st++;
-        }
-        cout<<endl;
-    }
-}
-
-void STACK::push(int a)
-{
-    lst.push_back(a);
-}
-
-void STACK::pop()
-{
-    lst.pop_back();
-}
-
+int displayqueue( queue <int>);
+int displaystack( stack <int>);
 int main()
 {
-    QUEUE s;
+	char choice;
+	int n;
+	stack <int>s;
+	queue <int>q;
 
-    s.display();
+	while(1)
+	{
+		cout<<"\n\n*** PROGRAM FOR IMPLEMENTATION OF STACK AND QUEUE USING STL ***"<<endl;
 
-    s.enqueue(5);
-    s.enqueue(1);
-    s.enqueue(10);
+		cout<<"(1) Stack Operations \n(2) Queue Operations"<<endl;
+		
+		cout<<"Enter your choice"<<endl;
+		cin>>choice;
+		if(choice=='1')
+		{
+			while(1)
+			{
+				cout<<"\nYou are in stack\n";
+				cout<<"(1) push \n(2) pop \n(3) size \n(4) display \n(5) top \n(6) exit  "<<endl;
+				cout<<"Enter your choice"<<endl;
+				cin>>choice;
 
-    s.display();
+				switch(choice)
+				{
+					case '1':
+						cout<<"\nenter the number to push\n";
+						cin>>n;
+						s.push(n);
+						break;
+					case '2':
+						if(s.empty()==1)
+						{
+							cout<<"\nstack is empty\n";
+							break;
+						}
+						else
+						{
+							s.pop();
+							cout<<"\nelement popped successfully\n";
+							break;
+						}
+					case '3':
+						cout<<"\nSize of stack is "<<s.size()<<endl;
+						break;
+					case '4':
+						displaystack(s);
+						break;
+					case '5':
+						cout<<"\nTop of stack is "<<s.top()<<endl;
+						break;
+					default:
+						cout<<"\nout of the stack now....\n\n";
+						goto exit;
+						break;
+				}
+			}
+		}
+		else if(choice=='2')
+		{
+			while(1)
+			{
+				cout<<"\nYou are in queue\n";
+				cout<<"(1) add \n(2) remove \n(3) size \n(4) display \n(5) front \n(6) end \n(7) exit  "<<endl;
+				cout<<"Enter your choice"<<endl;
+				cin>>choice;
 
-    s.dequeue();
+				switch(choice)
+				{
+					case '1':
+						cout<<"\nenter the number to add\n";
+						cin>>n;
+						q.push(n);
+						break;
+					case '2':
+						if(q.empty()==1)
+						{
+							cout<<"\nqueue is empty\n";
+							break;
+						}
+						else
+						{
+							q.pop();
+							cout<<"\nelement removed successfully\n";
+							break;
+						}
+					case '3':
+						cout<<"\nSize of queue is "<<q.size()<<endl;
+						break;
+					case '4':
+						displayqueue(q);
+						break;
+					case '5':
+						cout<<"\nFront of queue is "<<q.front()<<endl;
+						break;
+					case '6':
+						cout<<"\nEnd of queue is "<<q.back()<<endl;
+						break;
+					default:
+						cout<<"\nout of the queue now....\n\n";
+						goto exit;
+						break;
+				}
+			}
+		}
+		else
+		{
+			cout<<"\nwrong choice\n";
+		}
+exit:
+		cout<<"\n Do you want to continue(Y/N)\n";
+		cin>>choice;
+		if(choice=='y')
+			choice='Y';
+		if (choice!='Y')
+			break;
+	}
+}
 
-    s.display();
+int displaystack(stack <int>s)
+{
+	while(!s.empty())
+	{
+		cout<<"    "<<s.top()<<endl;
+		s.pop();
+	}
+	return 0;
+}
+
+int displayqueue(queue <int>q)
+{
+	while(!q.empty())
+	{
+		cout<<"    "<<q.front()<<endl;
+		q.pop();
+	}
+	return 0;
 }
